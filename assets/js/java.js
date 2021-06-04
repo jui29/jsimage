1da382b340b90fb48d4dc396923ebac7b82aca29
jui29 <jui29>
2021-06-05 03:10:40 +0600

    first commit

---
+++ assets/js/java.js
@@ -0,0 +1,24 @@
+let btn1=document.querySelector('#btn-1');
+let btn2=document.querySelector('#btn-2');
+let btn3=document.querySelector('#btn-3');
+
+
+btn1.addEventListener('click', () => {
+
+document.body.style.backgroundImage = "url('assets/img/3.jpg')";
+
+});
+
+
+btn2.addEventListener('click', () => {
+
+    document.body.style.backgroundImage = "url('assets/img/2.jpg')";
+    
+    });
+
+
+    btn3.addEventListener('click', () =>{
+
+        document.body.style.backgroundImage = "url('assets/img/3.jpg')";
+        
+        });
